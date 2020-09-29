525a764f39d8db5ec082f14f8c9869182aadadc9
Xarizmo <mpolikarpoff@gmail.com>
2020-09-29 22:18:17 +0300

    first commit

---
+++ src/PointerLockControls.js
@@ -0,0 +1,24 @@
+import React, { useEffect, useRef } from 'react';
+import { PointerLockControls as PointerLockControlsImpl } from 'three/examples/jsm/controls/PointerLockControls';
+import { extend, useThree } from 'react-three-fiber';
+
+extend({ PointerLockControlsImpl })
+
+export const PointerLockControls = (props) => {
+  const { camera, gl } = useThree();
+  const controls = useRef();
+  
+  useEffect(() => {
+    document.addEventListener('click', () => {
+      controls.current.lock()
+    })
+  }, [])
+  
+  return (
+    <pointerLockControlsImpl
+      ref={controls}
+      args={[camera, gl.domElement]}
+      {...props}
+    />
+  );
+}
